3e9e025ecc3dd15fae177b1536e26feb1fe6dc9e
0xCircuit <128435685+0zxx@users.noreply.github.com>
2023-03-21 02:50:32 +0000

    Create htmlident.py
    Install: python3 python3-pip
    run: pip3 install BeautifulSoup
    This script make one identation in your html code from locally file or remote get via url

---
+++ python/htmlident.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+# prompt the user for the input source
+input_source = input("Enter the path to the HTML file or a URL to fetch the HTML from: ")
+
+# check if the input source is a URL
+if input_source.startswith('http'):
+    # fetch the HTML from the URL
+    response = requests.get(input_source)
+    html = response.text
+else:
+    # read in the HTML file
+    with open(input_source, 'r') as f:
+        html = f.read()
+
+# parse the HTML using BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
+
+# write out the indented code to a new file
+output_path = input("Enter the path to the output file: ")
+with open(output_path, 'w') as f:
+    f.write(soup.prettify())
