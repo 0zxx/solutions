3a874c2aab83cad0aafe52bc21ce6ee1d753540f
0xCircuit <128435685+0zxx@users.noreply.github.com>
2023-03-21 03:03:00 +0000

    Update codefix.py
    now this run install and prompt if the user need remove or keep the lib

--- python/codefix.py
+++ python/codefix.py
@@ -1,3 +1,13 @@
+import importlib
+import subprocess
+
+# check if autopep8 is installed
+try:
+    importlib.import_module('autopep8')
+except ImportError:
+    # install autopep8 using pip
+    subprocess.run(['pip', 'install', 'autopep8'])
+
 import autopep8
 
 # Path of File
@@ -7,7 +17,7 @@ nome_arquivo = input("Insira o nome do arquivo ou caminho completo do arquivo: "
 with open(nome_arquivo, 'r') as arquivo:
     codigo = arquivo.read()
 
-# fix the code opening the lib of autopep8
+# fix the code using the autopep8 library
 codigo_corrigido = autopep8.fix_code(codigo)
 
 # rewrite the code with the fixed code
@@ -15,3 +25,9 @@ with open(nome_arquivo, 'w') as arquivo:
     arquivo.write(codigo_corrigido)
 
 print(f"O arquivo {nome_arquivo} foi corrigido com sucesso.")
+
+# prompt the user to choose whether to keep the libraries installed at the beginning
+keep_libraries = input("Deseja manter as bibliotecas instaladas no início? (s/n): ")
+if keep_libraries.lower() == 'n':
+    # uninstall autopep8 using pip
+    subprocess.run(['pip', 'uninstall', '-y', 'autopep8'])
