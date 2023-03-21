86d04b43920086509af4ca69bf1a967ec61f2c4a
0xCircuit <128435685+0zxx@users.noreply.github.com>
2023-03-21 03:48:08 +0000

    Run direct: python3 codefix.py codeissued.py

--- python/codefix.py
+++ python/codefix.py
@@ -1,33 +1,31 @@
 import importlib
 import subprocess
 
-# check if autopep8 is installed
-try:
-    importlib.import_module('autopep8')
-except ImportError:
-    # install autopep8 using pip
-    subprocess.run(['pip', 'install', 'autopep8'])
+def fix_code(file_name):
+    # check if autopep8 is installed
+    try:
+        importlib.import_module('autopep8')
+    except ImportError:
+        # install autopep8 using pip
+        subprocess.run(['pip', 'install', 'autopep8'])
 
-import autopep8
+    import autopep8
 
-# Path of File
-nome_arquivo = input("Insira o nome do arquivo ou caminho completo do arquivo: ")
+    # Opening file and learning content
+    with open(file_name, 'r') as file:
+        code = file.read()
 
-# Opening file and learning content
-with open(nome_arquivo, 'r') as arquivo:
-    codigo = arquivo.read()
+    # fix the code using the autopep8 library
+    fixed_code = autopep8.fix_code(code)
 
-# fix the code using the autopep8 library
-codigo_corrigido = autopep8.fix_code(codigo)
+    # rewrite the code with the fixed code
+    with open(f"{file_name.split('.')[0]}_fix.py", 'w') as file:
+        file.write(fixed_code)
 
-# rewrite the code with the fixed code
-with open(nome_arquivo, 'w') as arquivo:
-    arquivo.write(codigo_corrigido)
+    print(f"O arquivo {file_name} foi corrigido com sucesso e salvo como {file_name.split('.')[0]}_fix.py")
 
-print(f"O arquivo {nome_arquivo} foi corrigido com sucesso.")
-
-# prompt the user to choose whether to keep the libraries installed at the beginning
-keep_libraries = input("Deseja manter as bibliotecas instaladas no início? (s/n): ")
-if keep_libraries.lower() == 'n':
-    # uninstall autopep8 using pip
-    subprocess.run(['pip', 'uninstall', '-y', 'autopep8'])
+    # prompt the user to choose whether to keep the libraries installed at the beginning
+    keep_libraries = input("Deseja manter as bibliotecas instaladas no início? (s/n): ")
+    if keep_libraries.lower() == 'n':
+        # uninstall autopep8 using pip
+        subprocess.run(['pip', 'uninstall', '-y', 'autopep8'])
